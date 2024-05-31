b6c7137c7ed442f06ca6e380b686d517352f6d14
An <vnmodder@gmail.com>
2024-05-31 20:10:44 +0700

    update table header

--- squirrel-fe/src/components/admin/commons/TableCommon.vue
+++ squirrel-fe/src/components/admin/commons/TableCommon.vue
@@ -2,29 +2,26 @@
     <table v-if="config && config?.headers?.length" class="table bg-white rounded shadow-sm table-hover">
         <thead>
             <tr class="primary-bg  text-white ">
-                <th scope="col" width="50" class="text-center" v-if="config.options?.showRowHeader">#</th>
+                <th width="50" class="text-center align-middle" v-if="config.options?.showRowHeader">#</th>
                 <template v-for="h in config?.headers" :key="h.name">
-                    <th class="text-center" v-if="!h.hidden" scope="col">
-                        <span>
-                            {{ h.name }}
-                        </span>
-                        <div class="row" v-if="h.columnStyle == ColumnStyle.checkbox && h.attribute?.showCheckAll">
+                    <th class="text-center align-middle" v-if="!h.hidden" >
+                        <div class="row " v-if="h.columnStyle == ColumnStyle.checkbox && h.attribute?.showCheckAll">
                             <input type="checkbox" @change="(e)=>checkAllChanged(e,h)" />
                         </div>
+                        {{ h.name }}
                     </th>
                 </template>
-                <th class="text-center"
-                    v-if="config.options?.showDel || config.options?.showEdit || config.options?.showDetail"
-                    scope="col">Tùy chỉnh</th>
+                <th class="text-center align-middle"
+                    v-if="config.options?.showDel || config.options?.showEdit || config.options?.showDetail">Tùy chỉnh</th>
             </tr>
         </thead>
         <tbody v-if="items?.length">
             <tr v-for="(row, index1) in items" :key="index1" :class="index1 % 2 == 1 ? 'event' : ''"
                 @click="config.options?.rowSlected(row, index1 + startIndex)">
-                <td class="text-center" scope="row" v-if="config.options?.showRowHeader">{{ index1 + startIndex + 1 }}
+                <td class="text-center align-middle" v-if="config.options?.showRowHeader">{{ index1 + startIndex + 1 }}
                 </td>
                 <template v-for="(h, index) in config?.headers" :key="index">
-                    <td v-if="!h.hidden" :class="[
+                    <td v-if="!h.hidden" class="align-middle" :class="[
                         h.textAlign ? 'text-' + h.textAlign : 'text-left', h.className]" :width="h.width">
                         <span v-if="h.columnStyle == ColumnStyle.text || !h.columnStyle">
                             {{ row[h.id] }}
@@ -46,7 +43,7 @@
                     </td>
                 </template>
                 <td v-if="config.options?.showDel || config.options?.showEdit || config.options?.showDetail"
-                    :style="'text-align: center; cursor: pointer'">
+                    :style="'text-align: center; cursor: pointer'" class="align-middle">
                     <i v-if="config.options?.showDetail" class="fas fa-eye primary-text"
                         @click="config.options?.detailClick(row)"></i>
                     <i v-if="config.options?.showEdit" class="fas fa-pen ms-2"
