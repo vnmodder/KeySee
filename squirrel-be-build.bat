1ab8d6e7558a63e71f34b9d3cd14a141677a9ff4
An <vnmodder@gmail.com>
2024-05-31 08:10:33 +0700

    update script

--- squirrel-be-build.bat
+++ squirrel-be-build.bat
@@ -4,10 +4,20 @@ del /s /q .\publish\*
 
 dotnet publish -p:PublishProfile=Staging --output .\publish
 
+del /s /q .\publish\web.config
+
 %systemroot%\system32\inetsrv\appcmd stop apppool /apppool.name:"api.squirrel.io.vn"
 
-del /s /q C:\Host\Squirrel\api.squirrel.io.vn\*.!web.config
+for /R "C:\Host\Squirrel\api.squirrel.io.vn" %%f in (*.*) do (
+    if /I not "%%~nxf"=="web.config" del /s /f /q "%%f"
+)
 
-robocopy .\publish C:\Host\Squirrel\api.squirrel.io.vn /E /XF web.config
+for /f %%d in ('dir /ad /b C:\Host\Squirrel\api.squirrel.io.vn') do (
+    rd /s /q "C:\Host\Squirrel\api.squirrel.io.vn\%%d"
+)
+
+robocopy .\publish C:\Host\Squirrel\api.squirrel.io.vn /E
 
 %systemroot%\system32\inetsrv\appcmd start apppool /apppool.name:"api.squirrel.io.vn"
+
+exit 0 
